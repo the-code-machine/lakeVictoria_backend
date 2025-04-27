f0af61852bf58944da714c4c6ce1164b12c49872
sarthak <sarthakkhare821@gmail.com>
2025-04-27 20:36:45 +0530

    Update

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-5t-rowu_ta5ekk%_-37jz)j3_%k^6&#rx0zml49f4+oaalb!5b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.20.98.208', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['51.20.98.208', 'localhost', '127.0.0.1','api.caplock.in']
 
 
 # Application definition
@@ -45,6 +45,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://lakevictoria.vercel.app",
+    "https://api.caplock.in"
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
