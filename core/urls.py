c5f56425b339f5f7634b191fb4c01eda6eb94d4c
sarthak <sarthakkhare821@gmail.com>
2025-04-27 19:08:17 +0530

    Update

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),  # add this
