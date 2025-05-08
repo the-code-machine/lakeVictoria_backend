672c0476f1e796abec47104afe3dce4b02883f53
sarthak <sarthakkhare821@gmail.com>
2025-05-08 12:38:54 +0530

    Update

--- gallery/serializers.py
+++ gallery/serializers.py
@@ -5,7 +5,7 @@ from .models import GalleryPage, GalleryItem
 class GalleryItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = GalleryItem
-        fields = ['id', 'src', 'caption', 'date', 'type', 'thumbnail']
+        fields = ['id', 'src', 'caption', 'date', 'type',]
 
 class GalleryPageSerializer(serializers.ModelSerializer):
     items = GalleryItemSerializer(many=True, read_only=True)
