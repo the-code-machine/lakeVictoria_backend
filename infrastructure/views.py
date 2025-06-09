4d59e6b8089bbb8507fef4e94478146faec8fc84
Sarthak Khare <you@example.com>
2025-06-09 16:48:20 +0530

    update

--- infrastructure/views.py
+++ infrastructure/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
-
+from rest_framework.decorators import api_view
 class InfrastructureCombinedView(APIView):
     def get(self, request):
         return Response({
