fb9ff445007694852f057a3509f0179894274c49
Sarthak Khare <you@example.com>
2025-06-09 16:46:41 +0530

    update

--- infrastructure/urls.py
+++ infrastructure/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-    path('/vessel/<int:vessel_id>/', individual_vessel_view, name='individual-vessel'),
-from .views import InfrastructureCombinedView,vessel_fleet_view,
+from .views import InfrastructureCombinedView, vessel_fleet_view, individual_vessel_view
 
 urlpatterns = [
     path('', InfrastructureCombinedView.as_view(), name='infrastructure-all'),
-    path('/vessel-fleet/', vessel_fleet_view, name='vessel-fleet'),
-    path('/vessel/<int:vessel_id>/', individual_vessel_view, name='individual-vessel'),
-
+    path('vessel-fleet/', vessel_fleet_view, name='vessel-fleet'),
+    path('vessel/<int:vessel_id>/', individual_vessel_view, name='individual-vessel'),
 ]
